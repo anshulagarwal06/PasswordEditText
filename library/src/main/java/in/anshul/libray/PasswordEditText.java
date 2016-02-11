1f71c7d5596c7918519c248aa80b4f5dd6ac30e6
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2016-02-11 20:52:01 +0530

    Added Project to github

---
+++ library/src/main/java/in/anshul/libray/PasswordEditText.java
@@ -0,0 +1,170 @@
+package in.anshul.libray;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.EditText;
+
+/**
+ * Created by Shine on 09/02/16.
+ */
+public class PasswordEditText extends EditText {
+
+    private final int EXTRA_TOUCH_AREA = 50;
+    private Drawable mHideDrawable;
+    private Drawable mShowDrawable;
+    private boolean mPasswordVisible = false;
+    private boolean touchDown;
+
+
+    public PasswordEditText(Context context) {
+        super(context);
+    }
+
+    public PasswordEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+
+    public PasswordEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @TargetApi(21)
+    public PasswordEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        Resources res = context.getResources();
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.password_edit_text);
+
+        mHideDrawable = a.getDrawable(R.styleable.password_edit_text_hide_drawable);
+        mShowDrawable = a.getDrawable(R.styleable.password_edit_text_show_drawable);
+        mPasswordVisible = a.getBoolean(R.styleable.password_edit_text_password_visible, false);
+        a.recycle();
+        mHideDrawable.setBounds(0, 0, mHideDrawable.getIntrinsicWidth(), mHideDrawable.getIntrinsicHeight());
+        mShowDrawable.setBounds(0, 0, mShowDrawable.getIntrinsicWidth(), mShowDrawable.getIntrinsicHeight());
+        if (mPasswordVisible) {
+            showPassword();
+        } else {
+            hidePassword();
+        }
+    }
+
+
+    private void togglePasswordView() {
+        if (mPasswordVisible) {
+            hidePassword();
+        } else {
+            showPassword();
+        }
+    }
+
+    private void showPassword() {
+        setCompoundDrawables(null, null, mShowDrawable, null);
+        setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        setSelection(getText().length());
+        mPasswordVisible = true;
+    }
+
+    private void hidePassword() {
+        setCompoundDrawables(null, null, mHideDrawable, null);
+        setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        setSelection(getText().length());
+        mPasswordVisible = false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final int right = getRight();
+        final int drawableSize = getCompoundPaddingRight();
+        final int x = (int) event.getX();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (x + EXTRA_TOUCH_AREA >= right - drawableSize && x <= right + EXTRA_TOUCH_AREA) {
+                    touchDown = true;
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (x + EXTRA_TOUCH_AREA >= right - drawableSize && x <= right + EXTRA_TOUCH_AREA && touchDown) {
+                    touchDown = false;
+                    togglePasswordView();
+                    return true;
+                }
+                touchDown = false;
+                break;
+
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Parcelable saveState = super.onSaveInstanceState();
+        return new SavedState(saveState, mPasswordVisible);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+
+        SavedState savedState = (SavedState) state;
+
+        mPasswordVisible = savedState.isPasswordVisible();
+        if (mPasswordVisible) {
+            showPassword();
+        } else {
+            hidePassword();
+        }
+        super.onRestoreInstanceState(savedState.getSuperState());
+    }
+
+    private static class SavedState extends BaseSavedState {
+
+        private boolean isPasswordVisible = false;
+
+        public SavedState(Parcel source) {
+            super(source);
+            isPasswordVisible = source.readByte() != 0;
+        }
+
+        public SavedState(Parcelable superState, boolean passwordVisible) {
+            super(superState);
+            isPasswordVisible = passwordVisible;
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeByte((byte) (isPasswordVisible == true ? 1 : 0));
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR = new Creator<SavedState>() {
+
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+
+        };
+
+        public boolean isPasswordVisible() {
+            return isPasswordVisible;
+        }
+    }
+}
